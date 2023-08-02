77d516fa8efef540b188a5b7b6d6fb9bfebdd6be
Евгений Белов <john32-86@yandex.ru>
2023-08-02 13:17:30 +0300

    Задача - рекурсия

--- SocSeti/src/Day18/Task01/Main.java
+++ SocSeti/src/Day18/Task01/Main.java
@@ -2,7 +2,7 @@ package Day18.Task01;
 
 public class Main {
     public static void main(String[] args) {
-        int[] numbers = {5, 10, 25, 30, 35};
+        int[] numbers = {5, 11, 25, 31, 35};
         System.out.println(recursionSum(numbers, numbers.length));
     }
     public static int recursionSum(int[] arr, int counter) {
