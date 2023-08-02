2641b7225b7d72f03560a3cce5aa0035f61aeb90
Евгений Белов <john32-86@yandex.ru>
2023-08-02 14:10:47 +0300

    Задача - рекурсия Task02 доработка

--- SocSeti/src/Day18/Task02/Main.java
+++ SocSeti/src/Day18/Task02/Main.java
@@ -2,15 +2,15 @@ package Day18.Task02;
 
 public class Main {
     public static void main(String[] args) {
-        int number = 475772797;
+        int number = 575770797;
         System.out.println(count7(number));
     }
     public static int count7(int n) {
         int i = 1;
-        if (n / 10 != 7 & n % 10 != 7) {
+        if ( n % 10 != 7) {
             i = 0;
         }
-        if (n / 10 < 7 & n < 10){
+        if (n != 7 & n < 10){
             return 0;
         }
         return i + count7(n / 10);
