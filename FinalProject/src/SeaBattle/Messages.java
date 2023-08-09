fe050a057be95608b897f615aaafa54aed47a017
Evgeny Belov <john32-86@yandex.ru>
2023-08-09 20:49:13 +0300

    сохранения 20:55

--- FinalProject/src/SeaBattle/Messages.java
+++ FinalProject/src/SeaBattle/Messages.java
@@ -10,7 +10,7 @@ public class Messages {
 
     public static  void messagePrint4() {
         String message = "Введите координаты 4х палубного корабля" + "\n" +
-                "формат ввода: x,y;x,y;x,y;x,y";
+                "формат ввода: x,y;x,y;x,y;x,y 7";
         System.out.println(message);
     }
 }
